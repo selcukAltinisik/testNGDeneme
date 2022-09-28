e8f2bbd73629030d0117adafb6fa6a1d9507ddb5
selcukAltinisik <selcukaltin3544@gmail.com>
2022-09-29 00:15:48 +0300

    29Eyl22-00:15

--- src/test/java/tests/day17/C03_SoftAssert.java
+++ src/test/java/tests/day17/C03_SoftAssert.java
@@ -1,5 +1,4 @@
 package tests.day17;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -7,30 +6,38 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import utilities.TestBaseBeforeClassAfterClass;
-
 public class C03_SoftAssert extends TestBaseBeforeClassAfterClass {
-
     @Test
     public void test01() {
-        SoftAssert softassert = new SoftAssert();
+
+        /*
+        SoftAssert baslangic ve bitis satirlari arasindaki tum assertion'lari yapip
+        bitis olarak belirtmemiz gereken assertAll() methoduyla test methodumuzdaki
+        butun assertion'lari kontrol eder. Failed olan olursa assertion yaptigimiz
+        methodun sonuna yazdigimiz mesaji bize konsolda verir.
+        */
+
+        SoftAssert softAssert = new SoftAssert();
         //1-amazon anasayfaya gidin
         driver.get("https://amazon.com");
 
         //2-title in Amazon içerdigini test edin
-        softassert.assertTrue(driver.getTitle().contains("Amazon"),"Girdiginiz kelimeyi icermiyor");
+        softAssert.assertTrue(driver.getTitle().contains("Amazon"),"Gırdıgınız kelımeyı ıcermıyor.".toUpperCase());
 
         //3-arama kutusnun erişilebilir oldugunu tets edin
         WebElement aramaKutusu = driver.findElement(By.id("twotabsearchtextbox"));
-        softassert.assertTrue(aramaKutusu.isEnabled(),"WE erisilemez.");
+        softAssert.assertTrue(aramaKutusu.isEnabled(),"WE erısılemez".toUpperCase());
 
         //4-arama kutusuna Nuella yazıp aratın
         aramaKutusu.sendKeys("Nutella", Keys.ENTER);
 
         //5-arama yapıldıgını test edin
-        WebElement sonuc = driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
-        softassert.assertTrue(sonuc.isDisplayed(),"sonucWE goruntulenemedi.");
+        WebElement sonuc = driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
+        softAssert.assertTrue(sonuc.isDisplayed(),"SonucWE goruntulenemedı".toUpperCase());
 
         //6-arama sonucunun Nutella içerdigini test edin
-        softassert.assertTrue(sonuc.getText().contains("Nutella"),"Nutella icermiyor.");
+        softAssert.assertTrue(sonuc.getText().contains("Nutella"),"Nutella ıcermıyor".toUpperCase());
+        softAssert.assertAll();
+        System.out.println("Hata varsa burası calısmaz, hata yok kı burası calıstı".toUpperCase());
     }
 }
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,49 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Driver {
+
+    static WebDriver driver;
+
+    public static WebDriver getDriver(){
+        // testlerimizi calistirdigimizda her seferinde yeni driver oludturdugu icin
+        // her test methodu icin yeni bir pencere (driver) aciyor
+        // eger driver'a bir deger atanmamissa yani driver == null ise
+        // bir kere driver'i calistir diyerek bir kere if icini calistiracak
+        // ve driver artik bir kere calistigi icin ve deger atandigi icin null olmayacak
+        // ve direk return edecek ve diger testlerimiz ayni pencere (driver) uzerinde calisacak
+
+        if (driver==null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+
+            }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        return driver;
+    }
+
+    public static void  closeDriver(){
+
+        if (driver!=null){  //  driver'a değer atanmışsa kapat
+            driver.close();
+            driver=null;    //  Kapandıktan sonra sonraki açmaları garanti altına almak için driver'ı tekrar null yaptık.
+        }
+
+    }
+
+    @Test
+    public void quitDriver() {
+        if (driver!=null){
+            driver.quit();
+            driver=null;
+        }
+
+    }
+}
