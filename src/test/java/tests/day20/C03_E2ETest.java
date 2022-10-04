c667f6bb409d5f4535f44dca660cc1055605f15f
selcukAltinisik <selcukaltin3544@gmail.com>
2022-10-04 14:30:00 +0300

    04Eki22-14:29

--- src/test/java/tests/day20/C03_E2ETest.java
+++ src/test/java/tests/day20/C03_E2ETest.java
@@ -42,7 +42,7 @@ public class C03_E2ETest {
                 .sendKeys(Keys.TAB).sendKeys(Keys.SPACE).sendKeys(Keys.TAB).sendKeys(Keys.TAB)
                 .sendKeys(Keys.ENTER).perform();
 
-        //“RoomReservation was inserted successfully” textinin göründüğünü test edin...
+        //“RoomReservation was inserted successfully” textinin göründüğünü test edin....
         Thread.sleep(3000);
         Assert.assertTrue(hmcPage.roomReservationText.isDisplayed());
         //OK butonuna tıklayın.
