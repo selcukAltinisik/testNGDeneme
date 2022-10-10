430ec53aef19d4646fd73cd7431e79ca22db2290
selcukAltinisik <selcukaltin3544@gmail.com>
2022-10-10 16:03:41 +0300

    10Eki22-16:03

--- src/test/java/tests/practice08/P01.java
+++ src/test/java/tests/practice08/P01.java
@@ -16,23 +16,24 @@ public class P01 {
     public void test01() throws InterruptedException {
         //Go to https://testpages.herokuapp.com/styled/events/javascript-events.html
         Driver.getDriver().get("https://testpages.herokuapp.com/styled/events/javascript-events.html");
+
         //Click all the buttons and verify they are all clicked
         herokuTestPage = new HerokuTestPage();
+
         //kodlarımız yazarken clean code kapsamında daha sade kod yazabilmek amacı ile her test methodu içerisinde
         //ayrı bir object create etmektense bu objecti class seviyesinde instance olarak create edip
         //test methodlarında buna değer atamak ve kullanmak daha uygun bir yöntemdir
         herokuTestPage.onblur.click();
         herokuTestPage.onclick.click();
+
         herokuTestPage.onclick.click();
+
         actions.contextClick(herokuTestPage.contextmenu).
                 doubleClick(herokuTestPage.doubleClick).
                 click(herokuTestPage.onfocus).
-                click(herokuTestPage.keydown)
-                .sendKeys(Keys.ENTER).
-                click(herokuTestPage.keyup).
-                sendKeys(Keys.ENTER).
-                click(herokuTestPage.keypress).
-                sendKeys(Keys.ENTER).
+                click(herokuTestPage.keydown).sendKeys(Keys.ENTER).
+                click(herokuTestPage.keyup).sendKeys(Keys.ENTER).
+                click(herokuTestPage.keypress).sendKeys(Keys.ENTER).
                 moveToElement(herokuTestPage.mouseOver).
                 moveToElement(herokuTestPage.mouseLeave).
                 moveToElement(herokuTestPage.mouseOver).
@@ -43,5 +44,8 @@ public class P01 {
     public void test02() {
         List<WebElement> clicked = herokuTestPage.eventTriggered;
         Assert.assertEquals(clicked.size(), 11);
+        Driver.closeDriver();
+
     }
+
 }
