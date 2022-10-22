a0ee297a7c83cb75a372a9dd26aa0a119ba8eab3
selcukAltinisik <selcukaltin3544@gmail.com>
2022-10-23 00:28:00 +0300

    23Eki22-00:27

--- src/test/java/utilities/Listeners.java
+++ src/test/java/utilities/Listeners.java
@@ -4,6 +4,8 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import java.io.IOException;
+
 //  UTILITY CLASS
 public class Listeners implements ITestListener {
 
@@ -25,16 +27,34 @@ public class Listeners implements ITestListener {
     @Override
     public void onTestSuccess(ITestResult result) {
         System.out.println("onTestSuccess - PASS olan method sayisi kadar, ve PASS edilen methodlardan SONRA 1 kez calisir => "+result.getName());
+        try {
+            ReusableMethods.getScreenshot(result.getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
         System.out.println("onTestSkipped - Skipped edilen method sayisi kadar, ve skipped edilen methodlardan SONRA 1 kez calisir => "+result.getName());
+        try {
+            ReusableMethods.getScreenshot(result.getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println("onTestFailure - Failed edilen method sayisi kadar, ve Failed edilen methodlardan SONRA 1 kez calisir => "+result.getName());
+//        System.out.println("onTestFailure - Failed edilen Method sayisi kadar, ve Failed edilen methodlardan SONRA bir kez calisir => "+result.getName());
+
+        try {
+            ReusableMethods.getScreenshot(result.getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
